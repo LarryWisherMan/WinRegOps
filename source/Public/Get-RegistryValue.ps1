dbec205ae4d41b4ac394e334b1cac9d25fa44b18
LarryWisherMan <122727559+LarryWisherMan@users.noreply.github.com>
2024-09-07 14:43:38 -0700

    Feature/doc updates (#2)
    * docs(WinRegOps): :memo: Add comment-based help to public functions
    
    Added detailed comment-based help for all public functions in the WinRegOps module. This provides better guidance for users and improves module usability.
    
    * build(coverage): :construction_worker: Exclude WisherTools.Helpers from code coverage in build.yaml
    
    Updated the build configuration to exclude the WisherTools.Helpers folder from code coverage. These helper functions are not critical for coverage metrics.

--- source/Public/Get-RegistryValue.ps1
+++ source/Public/Get-RegistryValue.ps1
@@ -1,3 +1,28 @@
+<#
+.SYNOPSIS
+Retrieves a value from a specified registry key.
+
+.DESCRIPTION
+This function retrieves a specific value from a registry key. It returns the value if found, or $null if the value does not exist.
+
+.PARAMETER Key
+The registry key object from which the value will be retrieved.
+
+.PARAMETER ValueName
+The name of the value to retrieve from the registry key.
+
+.EXAMPLE
+$key = Open-RegistryKey -RegistryPath 'HKLM\Software\MyApp'
+Get-RegistryValue -Key $key -ValueName 'Setting'
+
+Retrieves the value 'Setting' from the registry key 'HKLM\Software\MyApp'.
+
+.OUTPUTS
+System.Object
+
+.NOTES
+#>
+
 function Get-RegistryValue {
     param (
         [Microsoft.Win32.RegistryKey]$Key,
