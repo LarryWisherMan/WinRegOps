df69bd831689899acc740c56d56746e052dc2781
LarryWisherMan <MikeRow@upkep.net>
2024-09-07 15:09:19 -0700

    test(WinRegOps): :white_check_mark: Add unit test skeletons for all public functions
    Added unit test skeletons for each public function in the WinRegOps module. These test files have been created but not yet defined with actual test cases. This prepares the module for future testing implementation.

---
+++ tests/Unit/Public/Get-RegistryValue.tests.ps1
@@ -0,0 +1,22 @@
+BeforeAll {
+    $script:dscModuleName = "WinRegOps"
+
+    Import-Module -Name $script:dscModuleName
+
+    $PSDefaultParameterValues['InModuleScope:ModuleName'] = $script:dscModuleName
+    $PSDefaultParameterValues['Mock:ModuleName'] = $script:dscModuleName
+    $PSDefaultParameterValues['Should:ModuleName'] = $script:dscModuleName
+}
+
+AfterAll {
+    $PSDefaultParameterValues.Remove('InModuleScope:ModuleName')
+    $PSDefaultParameterValues.Remove('Mock:ModuleName')
+    $PSDefaultParameterValues.Remove('Should:ModuleName')
+
+    # Unload the module being tested so that it doesn't impact any other tests.
+    Get-Module -Name $script:dscModuleName -All | Remove-Module -Force
+}
+
+Describe 'Get-RegistryValue.tests.ps1 Tests' -Tag 'Public' {
+
+}
